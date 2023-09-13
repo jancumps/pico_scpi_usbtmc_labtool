157b6a7df04ebf659a611baeedfe2666f0e4d871
Jan Cumps <jcumps@gmail.com>
2023-09-13 13:05:54 -0500

    compiles but USER_REGISTER_GROUP_DETAILS def wrong

--- source/scpi/scpi_user_config.h
+++ source/scpi/scpi_user_config.h
@@ -1,12 +1,28 @@
 #ifndef _SCPI_USER_CONFIG_H
 #define _SCPI_USER_CONFIG_H
 
-#define USE_CUSTOM_REGISTERS 1
-#define USER_REGISTERS
-#define USER_REGISTER_GROUPS
-#define USER_REGISTER_DETAILS
-#define USER_REGISTER_GROUP_DETAILS
-// TODO define custom status registers
 
+// define instrument specific registers
+#define USE_CUSTOM_REGISTERS 1
+#define USER_REGISTERS \
+        USER_REG_DIGINEVENT, /* Digital In event status Register */ \
+        USER_REG_DIGINEVENTE, /* Digital In event status enable Register */
+#define USER_REGISTER_GROUPS \
+        USER_REG_GROUP_DIGINEVENT,
+
+#define USER_REGISTER_DETAILS \
+    { SCPI_REG_CLASS_EVEN, USER_REG_GROUP_DIGINEVENT }, \
+    { SCPI_REG_CLASS_ENAB, USER_REG_GROUP_DIGINEVENT },
+
+#define USER_REGISTER_GROUP_DETAILS \
+{ \
+        USER_REG_DIGINEVENT, \
+        USER_REG_DIGINEVENTE, \
+        SCPI_REG_NONE, \
+        SCPI_REG_NONE, \
+        SCPI_REG_NONE, \
+        SCPI_REG_STB, \
+        STB_QES  /* TODO what value should I put here? */ \
+    }, /* USER_REG_GROUP_DIGINEVENT */
 
 #endif // _SCPI_USER_CONFIG_H
