fa65dbef4a223ff88474f1ea3a75f6eb4f9d5e87
Jan Cumps <jcumps@gmail.com>
2023-09-13 14:34:10 -0500

    for the moment, flag as Operational status bit

--- source/scpi/scpi_user_config.h
+++ source/scpi/scpi_user_config.h
@@ -4,9 +4,11 @@
 
 // define instrument specific registers
 #define USE_CUSTOM_REGISTERS 1
+
 #define USER_REGISTERS \
         USER_REG_DIGINEVENT, /* Digital In event status Register */ \
         USER_REG_DIGINEVENTE, /* Digital In event status enable Register */
+
 #define USER_REGISTER_GROUPS \
         USER_REG_GROUP_DIGINEVENT,
 
@@ -22,7 +24,7 @@
         SCPI_REG_NONE, \
         SCPI_REG_NONE, \
         SCPI_REG_STB, \
-        STB_QES  /* TODO what value should I put here? */ \
+        STB_OPS  /* TODO what value should I put here? */ \
     }, /* USER_REG_GROUP_DIGINEVENT */
 
 #endif // _SCPI_USER_CONFIG_H
