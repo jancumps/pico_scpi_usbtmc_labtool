76aa5df8556dccc2532649b075991e687b511e0c
Michael Stoops <3201502+michaelstoops@users.noreply.github.com>
2026-03-19 12:56:46 -0700

    Added overridable firmware version numbers

--- source/scpi/scpi-def.h
+++ source/scpi/scpi-def.h
@@ -11,7 +11,11 @@
 // serial number
 // pico unique id
 // version
+#ifdef FW_VER_STR
+#define SCPI_IDN4 FW_VER_STR
+#else
 #define SCPI_IDN4 "01.00"
+#endif
 
 extern const scpi_command_t scpi_commands[];
 
